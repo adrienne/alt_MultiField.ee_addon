e44c506480fdced5cccaa04b759f849ff21e7de6
Adrienne L. Travis <adrienne@utilitarienne.com>
2012-07-25 22:03:39 -0500

    Bumped version number

--- system/expressionengine/third_party/alt_multifield/alt_multifield_config.php
+++ system/expressionengine/third_party/alt_multifield/alt_multifield_config.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) { exit('No direct script access allowed'); }
 
 $config['name'] = 'ALT MultiField';
-$config['version'] = '1.0.7';
+$config['version'] = '1.0.8';
 $config['description'] = 'A Fieldtype to create small groupings of fields.';
 $config['docs_url'] = '';
